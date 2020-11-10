90fb9a5e80d86df347c9cab0e2271302dc13c2e6
Marek Szymański <szymanski_m@o2.pl>
2020-11-10 18:19:00 +0100

    Created ChatViewModel.kt, ChatFragment.kt and layout fragment_chat.xml for feature chat.

---
+++ app/src/main/java/com/szymanski/sharelibrary/features/chat/presentation/ChatFragment.kt
@@ -0,0 +1,7 @@
+package com.szymanski.sharelibrary.features.chat.presentation
+
+import androidx.fragment.app.Fragment
+
+class ChatFragment : Fragment() {
+
+}
---
+++ app/src/main/java/com/szymanski/sharelibrary/features/chat/presentation/ChatViewModel.kt
@@ -0,0 +1,9 @@
+package com.szymanski.sharelibrary.features.chat.presentation
+
+import com.szymanski.sharelibrary.core.base.BaseViewModel
+import com.szymanski.sharelibrary.core.exceptions.ErrorMapper
+
+class ChatViewModel(
+    private val errorMapper: ErrorMapper,
+) : BaseViewModel(errorMapper) {
+}
