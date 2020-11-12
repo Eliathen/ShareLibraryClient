00ade97f0a8123ced8cc9bc712131e108d8a7d62
Marek Szymański <szymanski_m@o2.pl>
2020-11-12 20:39:05 +0100

    Created UserNavigation.kt and UserNavigationImpl.kt

---
+++ app/src/main/java/com/szymanski/sharelibrary/features/users/navigation/UserNavigationImpl.kt
@@ -0,0 +1,24 @@
+package com.szymanski.sharelibrary.features.users.navigation
+
+import com.szymanski.sharelibrary.R
+import com.szymanski.sharelibrary.core.navigation.FragmentNavigator
+
+class UserNavigationImpl(
+    private val fragmentNavigator: FragmentNavigator,
+) : UserNavigation {
+    override fun openRegisterScreen() {
+        fragmentNavigator.navigateTo(R.id.action_navigate_from_login_screen_to_register_screen)
+    }
+
+    override fun openBooksScreen() {
+        fragmentNavigator.navigateTo(R.id.action_navigate_from_login_screen_to_book_screen)
+    }
+
+    override fun openLoginScreen() {
+        fragmentNavigator.navigateTo(R.id.login_screen)
+    }
+
+    override fun goBack() {
+        fragmentNavigator.goBack()
+    }
+}
