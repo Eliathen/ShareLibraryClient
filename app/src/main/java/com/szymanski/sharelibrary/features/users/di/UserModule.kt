a7978fd22d2466ec92e0bec28d3c1873ba7d6616
Marek Szymański <szymanski_m@o2.pl>
2020-11-12 20:49:09 +0100

    Added new dependencies to UserModule.kt

--- app/src/main/java/com/szymanski/sharelibrary/features/users/di/UserModule.kt
+++ app/src/main/java/com/szymanski/sharelibrary/features/users/di/UserModule.kt
@@ -2,7 +2,12 @@ package com.szymanski.sharelibrary.features.users.di
 
 import com.szymanski.sharelibrary.features.users.data.UserRepositoryImpl
 import com.szymanski.sharelibrary.features.users.domain.UserRepository
+import com.szymanski.sharelibrary.features.users.domain.usecase.LoginUserUseCase
 import com.szymanski.sharelibrary.features.users.domain.usecase.RegisterUserUseCase
+import com.szymanski.sharelibrary.features.users.login.presentation.LoginViewModel
+import com.szymanski.sharelibrary.features.users.navigation.UserNavigation
+import com.szymanski.sharelibrary.features.users.navigation.UserNavigationImpl
+import com.szymanski.sharelibrary.features.users.profile.presentation.ProfileViewModel
 import com.szymanski.sharelibrary.features.users.registration.presentation.RegisterViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -16,8 +21,21 @@ val userModule = module {
     factory {
         RegisterUserUseCase(get())
     }
+    factory {
+        LoginUserUseCase(get())
+    }
 
     viewModel {
-        RegisterViewModel(get())
+        RegisterViewModel(get(), get())
+    }
+    viewModel {
+        LoginViewModel(get(), get(), get())
+    }
+    factory<UserNavigation> {
+        UserNavigationImpl(get())
+    }
+
+    viewModel {
+        ProfileViewModel(get(), get())
     }
 }
