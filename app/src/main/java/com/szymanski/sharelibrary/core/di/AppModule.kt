94d128e76a9fc3d8aa7bacdbb6e8528289e4cff8
Marek Szymański <szymanski_m@o2.pl>
2020-11-12 20:49:25 +0100

    Added new dependencies to AppModule.kt

--- app/src/main/java/com/szymanski/sharelibrary/core/di/AppModule.kt
+++ app/src/main/java/com/szymanski/sharelibrary/core/di/AppModule.kt
@@ -1,7 +1,32 @@
 package com.szymanski.sharelibrary.core.di
 
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.szymanski.sharelibrary.R
+import com.szymanski.sharelibrary.core.exceptions.ErrorMapper
+import com.szymanski.sharelibrary.core.exceptions.ErrorMapperImpl
+import com.szymanski.sharelibrary.core.navigation.FragmentNavigator
+import com.szymanski.sharelibrary.core.navigation.FragmentNavigatorImpl
+import com.szymanski.sharelibrary.core.provider.ActivityProvider
+import org.koin.android.ext.koin.androidApplication
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val appModule = module {
+
+    factory { LinearLayoutManager(androidContext()) }
+
+    single(createdAtStart = true) {
+        ActivityProvider(androidApplication())
+    }
+    factory<FragmentNavigator> {
+        FragmentNavigatorImpl(
+            activityProvider = get(),
+            navHostFragmentRes = R.id.nav_host_fragment,
+            homeHostFragmentRes = R.id.books_screen
+        )
+    }
+    factory<ErrorMapper> {
+        ErrorMapperImpl()
+    }
 
 }
