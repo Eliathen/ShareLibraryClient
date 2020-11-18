0150a07888a1147434bc4a2eb22a87972fe22f74
Marek Szymański <szymanski_m@o2.pl>
2020-11-18 20:09:51 +0100

    Added property id in Author.kt and Book.kt

--- app/src/main/java/com/szymanski/sharelibrary/features/book/domain/model/Author.kt
+++ app/src/main/java/com/szymanski/sharelibrary/features/book/domain/model/Author.kt
@@ -3,6 +3,7 @@ package com.szymanski.sharelibrary.features.book.domain.model
 import java.io.Serializable
 
 class Author(
+    val id: Long?,
     val name: String?,
     val surname: String?,
 ) : Serializable
--- app/src/main/java/com/szymanski/sharelibrary/features/book/domain/model/Book.kt
+++ app/src/main/java/com/szymanski/sharelibrary/features/book/domain/model/Book.kt
@@ -2,13 +2,41 @@ package com.szymanski.sharelibrary.features.book.domain.model
 
 import java.io.Serializable
 
+
 data class Book(
+
+    val id: Long?,
 
     val title: String?,
 
     val authors: List<Author>?,
 
-    val image: ByteArray?,
-) : Serializable {
+    val cover: ByteArray?,
+
+    ) : Serializable {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Book
+
+        if (id != other.id) return false
+        if (title != other.title) return false
+        if (authors != other.authors) return false
+        if (cover != null) {
+            if (other.cover == null) return false
+            if (!cover.contentEquals(other.cover)) return false
+        } else if (other.cover != null) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id?.hashCode() ?: 0
+        result = 31 * result + (title?.hashCode() ?: 0)
+        result = 31 * result + (authors?.hashCode() ?: 0)
+        result = 31 * result + (cover?.contentHashCode() ?: 0)
+        return result
+    }
 
 }
