493086815837e8d6126860d39e67fc77c44c04fc
Marek Szymański <szymanski_m@o2.pl>
2020-11-18 20:13:56 +0100

    Added new parameter userId to BookRepository.saveBook()

--- app/src/main/java/com/szymanski/sharelibrary/features/book/domain/usecase/SaveBookUseCase.kt
+++ app/src/main/java/com/szymanski/sharelibrary/features/book/domain/usecase/SaveBookUseCase.kt
@@ -1,14 +1,16 @@
 package com.szymanski.sharelibrary.features.book.domain.usecase
 
 import com.szymanski.sharelibrary.core.base.BaseUseCase
+import com.szymanski.sharelibrary.core.storage.local.UserStorage
 import com.szymanski.sharelibrary.features.book.domain.BookRepository
 import com.szymanski.sharelibrary.features.book.domain.model.Book
 
 class SaveBookUseCase(
     private val bookRepository: BookRepository,
+    private val userStorage: UserStorage,
 ) : BaseUseCase<Unit, Book>() {
 
     override suspend fun action(params: Book) {
-        bookRepository.saveBook(params)
+        bookRepository.saveBook(params, userStorage.getUserId())
     }
 }
